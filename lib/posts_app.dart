d313174bc318bb0395a64d722675bbeffafa705e
Travis Tressler <r.travis.tressler@gmail.com>
2020-04-11 01:19:01 -0400

    Initial impl is finished

--- lib/posts_app.dart
+++ lib/posts_app.dart
@@ -1,19 +1,25 @@
 import 'package:flutter/material.dart';
-
+import 'package:provider/provider.dart';
+import 'package:provider_advanced/models/global_view_model.dart';
 import 'flows/postsflow/posts_flow.dart';
 
 
 const postsFlowRouteName = '/';
+
 class PostsApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Posts Application',
-      debugShowCheckedModeBanner: false,
-      initialRoute: postsFlowRouteName,
-      routes: {
-        postsFlowRouteName: (context) => PostsFlow(),
-      },
+    return ChangeNotifierProvider(
+      //no impl for GlobalViewModel, but this is how you could provide global dependencies
+      create: (context) => GlobalViewModel(),
+      child: MaterialApp(
+        title: 'Posts Application',
+        debugShowCheckedModeBanner: false,
+        initialRoute: postsFlowRouteName,
+        routes: {
+          postsFlowRouteName: (context) => PostsFlow(),
+        },
+      ),
     );
   }
 }
