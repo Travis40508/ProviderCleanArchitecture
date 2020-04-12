8ef04477f413755dd14eeb96cc5cbd28f4ada36b
Travis Tressler <r.travis.tressler@gmail.com>
2020-04-12 15:36:38 -0400

    Added one more screen navigation for context

--- lib/screens/postsscreen/posts_screen.dart
+++ lib/screens/postsscreen/posts_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:provider_advanced/models/viewmodels/posts_view_model.dart';
+import 'package:provider_advanced/screens/chucknorrisfactsscreen/chuck_norris_facts_route.dart';
 import 'package:provider_advanced/screens/editpostsscreen/edit_post_route.dart';
 
 
@@ -13,6 +14,12 @@ class PostsScreen extends StatelessWidget {
         backgroundColor: Colors.purpleAccent,
         centerTitle: true,
         title: Text('Posts'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.person),
+            onPressed: () => Navigator.pushNamed(context, ChuckNorrisFactsRoute.routeName),
+          )
+        ],
       ),
 
       body: Consumer<PostsViewModel>(
